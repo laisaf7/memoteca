0ff7ad903e3590448a3b8b1adc6f91b4c374b9d2
Laís Azevedo Freitas <137813392+laisaf7@users.noreply.github.com>
2026-09-21 12:18:24 -0300

    finalização do projeto

--- js/main.js
+++ js/main.js
@@ -18,7 +18,11 @@ async function manipularSubmisssaoFormulario(event) {
     const autoria = document.getElementById("pensamento-autoria").value
 
     try {
-        await api.salvarPensamento({conteudo, autoria})
+        if(id) {
+            await api.editarPensamento({id, conteudo, autoria})
+        } else {
+            await api.salvarPensamento({conteudo, autoria})
+        }
         ui.renderizarPensamentos()
     } catch (error) {
         alert("Erro ao salvar pensamento")
